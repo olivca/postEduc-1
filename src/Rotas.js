84e19c8f3e06710e2403f01b31ea6e15c3457cae
Gab Menacho <72589489+GabMenacho@users.noreply.github.com>
2021-01-13 17:40:40 -0300

    Adicionando a rota da página de categorias

--- src/Rotas.js
+++ src/Rotas.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import PaginaDeEvento from './Componentes/eventos/PaginaDeEventos'
 import EventoEspecifico from './Componentes/eventos/EventoEspecifico'
+import Categorias from './Componentes/eventos/categorias'
 
 export class Rotas extends React.Component{
     render(){
@@ -12,6 +13,7 @@ export class Rotas extends React.Component{
     
                 {/* Crie as Rotas Aqui */}
                 <Route exact path="/EventoEspecifico" component={EventoEspecifico}/>
+                <Route exact path="/categorias" component={Categorias}/>
             
             </Switch>
         )    
