53ce950b5e20cbe43daed50b40915ec39950023a
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-01-18 01:52:06 -0300

    Login - front-end e back-end e correção no Cadastro de usuário

--- src/Rotas.js
+++ src/Rotas.js
@@ -5,6 +5,7 @@ import PaginaDeEvento from './Componentes/eventos/PaginaDeEventos'
 import EventoEspecifico from './Componentes/eventos/EventoEspecifico'
 import CadastroEvento from './Componentes/eventos/criarEvento/CadastroEvento'
 import CadastroUsuario from './Componentes/CadastroUsuario/CadastroUsuario'
+import Login from './Componentes/Login/Login'
 
 export class Rotas extends React.Component{
     render(){
@@ -16,6 +17,7 @@ export class Rotas extends React.Component{
                 <Route exact path="/EventoEspecifico" component={EventoEspecifico}/>
                 <Route exact path="/CadastroEvento" component={CadastroEvento}/>
                 <Route exact path="/CadastroUsuario" component={CadastroUsuario}/>
+                <Route exact path="/Login" component={Login}/>
             
             </Switch>
         )    
