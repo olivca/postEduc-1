3a178928102179eafb208e2afae449cc1bce9a13
jeffersoncsw <je_wiltenburg@hotmail.com>
2021-01-19 16:14:39 -0300

    Ajuste na Formulario de Login e Cadastro

--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import {useState} from 'react';
-import {Form,Button,Col}  from 'react-bootstrap';
+import {Form,Button,Col, Jumbotron}  from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
 
@@ -68,31 +68,29 @@ export default function Login(props) {
     
         
     return(
-        <div className="form-group">
+        <Jumbotron className="m-0">
+            <div >
+                <h2 className="text-center">Login</h2><br />
+                <Form onSubmit={Envio}>
 
+                    <Col sm={12} md={{span: 6, offset: 3}} lg={{offset:4, span:4}}>
+                    <Form.Group>
+                        <Form.Label>Nome de usuário</Form.Label>
+                        <Form.Control  type="text" id="nome_usuario" name="nome_usuario" placeholder="Entre com o seu nome de usuário" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control  type="password" id="senha" name="senha" placeholder="Entre com sua senha" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">Entrar</Button>
+
+                    </Col>
+                </Form>
                 
-                <br /><h2>Login</h2><br />
-            
-
-            <Form onSubmit={Envio}>
-
-                <Col sm={12} md={6} lg={4}>
-                <Form.Group>
-                    <Form.Label>Nome de usuário</Form.Label>
-                    <Form.Control  type="text" id="nome_usuario" name="nome_usuario" placeholder="Entre com o seu nome de usuário" />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Control  type="password" id="senha" name="senha" placeholder="Entre com sua senha" />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">Entrar</Button>
-
-                </Col>
-            </Form>
-        </div>
-
+            </div>
+        </Jumbotron>
     )
 
     
