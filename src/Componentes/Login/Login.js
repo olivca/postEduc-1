9bb68429e4dd1a31867df4babc40869e8bdede7e
lucas <lucasd.viana1993@gmail.com>
2021-01-20 17:58:36 -0300

    atualizando pagina de login

--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form,Button,Col}  from 'react-bootstrap';
+import {Form,Button,Col,Jumbotron}  from 'react-bootstrap';
 import { Redirect } from 'react-router';
 
 import { connect } from 'react-redux';
@@ -15,10 +15,31 @@ class Login extends React.Component{
             redirect: false,
             nome:'',
             id:'',
-
+            nome_usuario:'',
+            senha:''
         }
 
         this.Envio = this.Envio.bind(this)
+        this.alteracao_user = this.alteracao_user.bind(this)
+        this.alteracao_senha = this.alteracao_senha.bind(this)
+    }
+
+    alteracao_user(evento){
+        evento.preventDefault()
+        let valor = evento.target.value
+        
+        this.setState({
+            'nome_usuario': valor
+       })        
+    };  
+
+    alteracao_senha(evento){
+        evento.preventDefault()
+        let valor = evento.target.value
+        
+        this.setState({
+            'senha': valor
+       })  
     }
 
     async Envio(evento){
@@ -52,28 +73,31 @@ class Login extends React.Component{
         }else{
             
             return(
-                <div className="form-group">   
-                        <br /><h2>Login:</h2><br />
-                    
+                <Jumbotron className="m-0">
+                    <div >
+                        <h2 className="text-center">Login</h2><br />
+                        <Form onSubmit={this.Envio}>
 
-                    <Form onSubmit={this.Envio}>
+                            <Col sm={12} md={{span: 6, offset: 3}} lg={{offset:4, span:4}}>
+                            <Form.Group>
+                                <Form.Label>Nome de usuário</Form.Label>
+                                <Form.Control onChange={this.alteracao_user} type="text" id="nome_usuario" name="nome_usuario" placeholder="Entre com o seu nome de usuário" />
+                                <Form.Control  type="text" id="nome_usuario" name="nome_usuario" placeholder="Entre com o seu nome de usuário" />
+                            </Form.Group>
 
-                        <Col sm={12} md={6} lg={4}>
-                        <Form.Group>
-                            <Form.Label>Nome de usuário</Form.Label>
-                            <Form.Control  type="text" id="nome_usuario" name="nome_usuario" placeholder="Entre com o seu nome de usuário" />
-                        </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control onChange={this.alteracao_senha} type="password" id="senha" name="senha" placeholder="Entre com sua senha" />
+                                <Form.Control  type="password" id="senha" name="senha" placeholder="Entre com sua senha" />
+                            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control  type="password" id="senha" name="senha" placeholder="Entre com sua senha" />
-                        </Form.Group>
+                            <Button variant="primary" type="submit">Entrar</Button>
 
-                        <Button variant="primary" type="submit" >Entrar</Button>
+                            </Col>
+                        </Form>
                         
-                        </Col>
-                    </Form>
-                </div>
+                    </div>
+                </Jumbotron>       
             )
     
         }
