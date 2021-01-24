e5547ae551ac0c288a5b8b977c51425a362a7b99
olivca <carolineol.jw@gmail.com>
2021-01-24 14:17:43 -0300

    ajuste infos evento

--- src/Componentes/eventos/Detalhes.js
+++ src/Componentes/eventos/Detalhes.js
@@ -21,10 +21,11 @@ class Detalhes extends React.Component{
             return (
                 <div>                
                 <h2>{this.props.nome}</h2>
+                <p>{this.props.modalidade}</p>
                 <p className="pt-5">Local: {this.props.logradouro},{this.props.numero},{this.props.bairro}</p>
                 <p className="pt-5">{this.props.complemento},{this.props.cidade},{this.props.estado}</p>
 
-                <p>Quando: {this.props.data} - {this.props.hora_evento}</p>
+                <p>Quando: {this.props.data} - {this.props.hora}</p>
                 <p>Duração: {this.props.carga}</p>
                 <Button as={Link} to={`/EditarEvento${this.props.id}`} >Editar</Button>
                 <Denuncia id={this.props.id}/>
@@ -37,8 +38,8 @@ class Detalhes extends React.Component{
             <p className="pt-5">Local: {this.props.logradouro},{this.props.numero},{this.props.bairro}</p>
             <p className="pt-5">{this.props.complemento},{this.props.cidade},{this.props.estado}</p>
 
-            <p>Quando: {this.props.data} - {this.props.hora_evento}</p>
-            <p>Duração: {this.props.carga_horaria}</p>
+            <p>Quando: {this.props.data} - {this.props.hora}</p>
+            <p>Duração: {this.props.carga}</p>
             <Denuncia id={this.props.id}/>
             </div>
         )
