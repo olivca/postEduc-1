a679f84b3569143ed01d7200d414c203711cf787
lucas <lucasd.viana1993@gmail.com>
2021-01-23 23:30:32 -0300

    favico

--- src/Componentes/eventos/Detalhes.js
+++ src/Componentes/eventos/Detalhes.js
@@ -25,7 +25,7 @@ class Detalhes extends React.Component{
                 <p className="pt-5">{this.props.complemento},{this.props.cidade},{this.props.estado}</p>
 
                 <p>Quando: {this.props.data} - {this.props.hora_evento}</p>
-                <p>Duração: {this.props.carga_horaria}</p>
+                <p>Duração: {this.props.carga}</p>
                 <Button as={Link} to={`/EditarEvento${this.props.id}`} >Editar</Button>
                 <Denuncia id={this.props.id}/>
                 </div>
